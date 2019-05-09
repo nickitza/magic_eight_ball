bb3dd7d32fc05741d579214f1614eb53ec43bf3f
Nicki Nixon <nickitza@gmail.com>
2019-05-09 14:09:56 -0600

    added responses to 8 ball

---
+++ magic_eight.rb
@@ -0,0 +1,30 @@
+require 'colorize'
+require 'pry'
+
+responses = [
+  "The spirits say absolutely not.",
+  "The spirits say absolutely.",
+  "The path is hazy; try again later.",
+  "The path is shrouded in darkness; don't pursue.",
+  "The path is clear to travel; pursue freely.",
+  "Concentrate deeply and ask again.",
+  "The spirits are unsure.",
+  "Most definitely.",
+  "Outlook not so good.",
+  "The fae say it's tricky, but yes.",
+  "The fae say it's tricky, but no.",
+  "You really don't want to know.",
+  "It's everything you've dreamed of.",
+  "You should ask your cat.",
+  "You already know the answer.",
+  "The runes say it's possible.",
+  "The scrying ball says it's likely.",
+  "The Ouija board spells Not Really.",
+  "Ask the ghost sitting next to you. It knows.",
+  "You will dream the answer tonight.",
+  "The demon next to you is nodding."
+]
+
+puts responses.sample
+puts responses.sample
+
